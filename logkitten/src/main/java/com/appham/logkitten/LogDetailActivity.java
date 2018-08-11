cbfe720fc493f47b1730033c569659280fecfb6a
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-08-11 13:48:43 +0100

    add LogDetailActivity

--- logkitten/src/main/java/com/appham/logkitten/LogDetailActivity.java
+++ logkitten/src/main/java/com/appham/logkitten/LogDetailActivity.java
@@ -1,7 +1,31 @@
 package com.appham.logkitten;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class LogDetailActivity extends AppCompatActivity {
-    //TODO: Show the details of the log
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_detail);
+
+        Intent intent = getIntent();
+
+        if (intent != null && Intent.ACTION_SEND.equals(intent.getAction()) && intent.getType() != null) {
+            if ("text/plain".equals(intent.getType())) {
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                if (sharedText != null) {
+                    TextView txtLogDetail = findViewById(R.id.txtLogDetail);
+                    txtLogDetail.setText(sharedText);
+                    txtLogDetail.setMovementMethod(new ScrollingMovementMethod());
+                }
+            }
+        }
+
+    }
 }
