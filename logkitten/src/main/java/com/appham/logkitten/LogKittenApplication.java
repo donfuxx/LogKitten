26f6cb53befeebd73d8d3d8ccf1d27fc0bc37b84
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-08-26 16:31:56 +0100

    custom exception handler with exception notification

--- logkitten/src/main/java/com/appham/logkitten/LogKittenApplication.java
+++ logkitten/src/main/java/com/appham/logkitten/LogKittenApplication.java
@@ -2,14 +2,42 @@ package com.appham.logkitten;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v4.content.ContextCompat;
 
+import com.appham.logkitten.notifications.NotificationFactory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
 public class LogKittenApplication extends Application {
+
+    private final int CRASH_ID = 41;
 
     @Override
     public void onCreate() {
         super.onCreate();
         ContextCompat.startForegroundService(this,
                 new Intent(this, LogKittenService.class));
+
+        Thread.UncaughtExceptionHandler defaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
+        Thread.setDefaultUncaughtExceptionHandler((paramThread, paramThrowable) -> {
+
+            StringWriter stringWriter = new StringWriter();
+            paramThrowable.printStackTrace(new PrintWriter(stringWriter));
+            String exceptionStr = stringWriter.toString();
+
+            LogEntry logEntry = new LogEntry("CRASH: " + new Date().toString(),
+                    Thread.currentThread().getId() + "",
+                    "E",
+                    exceptionStr);
+
+            NotificationFactory.newNotification(CRASH_ID, logEntry, this);
+
+            SystemClock.sleep(2000);
+            defaultExceptionHandler.uncaughtException(paramThread, paramThrowable);
+        });
     }
 }
