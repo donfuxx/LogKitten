1b385674e934f466b553859f87c29af62b615c16
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-08-29 20:28:53 +0100

    add unit test for valid warning log line

--- logkitten/src/test/java/com/appham/logkitten/LogEntryFactoryTest.java
+++ logkitten/src/test/java/com/appham/logkitten/LogEntryFactoryTest.java
@@ -12,8 +12,19 @@ public class LogEntryFactoryTest {
         LogEntry logEntry = LogEntryFactory.buildLogEntry(logline);
 
         Assert.assertEquals("", logEntry.getTime());
-        Assert.assertEquals("", logEntry.getLevel());
         Assert.assertEquals("", logEntry.getPid());
+        Assert.assertEquals("", logEntry.getLevel());
         Assert.assertEquals("", logEntry.getContent());
+    }
+
+    @Test
+    public void testBuildLogEntryWarningValid() {
+        String logline = "08-29 20:23:46.390  3397  3397 W System.err: java.lang.RuntimeException: Test Exception Demo";
+        LogEntry logEntry = LogEntryFactory.buildLogEntry(logline);
+
+        Assert.assertEquals("08-29 20:23:46.390", logEntry.getTime());
+        Assert.assertEquals("3397  3397", logEntry.getPid());
+        Assert.assertEquals("W", logEntry.getLevel());
+        Assert.assertEquals("System.err: java.lang.RuntimeException: Test Exception Demo", logEntry.getContent());
     }
 }
