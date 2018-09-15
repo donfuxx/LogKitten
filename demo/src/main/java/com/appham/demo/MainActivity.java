0988dc030e46f9578b322b77c8b7383a3074882d
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-09-15 14:42:01 +0100

    add test warning and error demo buttons

--- demo/src/main/java/com/appham/demo/MainActivity.java
+++ demo/src/main/java/com/appham/demo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.appham.demo;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
         Log.w("Demo", "Test Warning Demo");
 
-        new Handler().postDelayed(() -> {
-            try {
-                throw new RuntimeException("Test Exception Demo");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, 2000);
+    }
+
+    public void warning(View view) {
+        Log.w("Demo", "Test Warning Log Demo");
+    }
+
+    public void error(View view) {
+        Log.e("Demo", "Test Error Log Demo");
     }
 
     public void crash(View view) {
